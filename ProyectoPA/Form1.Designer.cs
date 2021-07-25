5b25e0e49d03ed727d33ff77e5755339886111ab
Elian Ramirez <75921651+elian-doctom@users.noreply.github.com>
2021-07-25 10:37:15 -0500

    Add files via upload

---
+++ ProyectoPA/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace ProyectoPA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelTitulo = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.bntIngresar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.panelTitulo.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panelLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 538);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.label4);
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 140);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Coop. De Buses Machala City";
+            // 
+            // panelTitulo
+            // 
+            this.panelTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.panelTitulo.Controls.Add(this.button3);
+            this.panelTitulo.Controls.Add(this.pictureBox2);
+            this.panelTitulo.Controls.Add(this.lblTitulo);
+            this.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitulo.Location = new System.Drawing.Point(220, 0);
+            this.panelTitulo.Name = "panelTitulo";
+            this.panelTitulo.Size = new System.Drawing.Size(834, 75);
+            this.panelTitulo.TabIndex = 1;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblTitulo.Location = new System.Drawing.Point(87, 25);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(58, 19);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "INICIO";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.panel2.BackgroundImage = global::ProyectoPA.Properties.Resources.frmFondo;
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.bntIngresar);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.panel2.Location = new System.Drawing.Point(220, 74);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(834, 464);
+            this.panel2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtContra);
+            this.groupBox1.Controls.Add(this.txtUsuario);
+            this.groupBox1.Location = new System.Drawing.Point(165, 150);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(504, 149);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(45, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "USUARIO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(45, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CONTRASENA:";
+            // 
+            // txtContra
+            // 
+            this.txtContra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.txtContra.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContra.ForeColor = System.Drawing.Color.White;
+            this.txtContra.Location = new System.Drawing.Point(187, 90);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.Size = new System.Drawing.Size(274, 22);
+            this.txtContra.TabIndex = 4;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.Color.White;
+            this.txtUsuario.Location = new System.Drawing.Point(187, 35);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(274, 22);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // bntIngresar
+            // 
+            this.bntIngresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.bntIngresar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntIngresar.ForeColor = System.Drawing.Color.Gainsboro;
+            this.bntIngresar.Location = new System.Drawing.Point(360, 333);
+            this.bntIngresar.Name = "bntIngresar";
+            this.bntIngresar.Size = new System.Drawing.Size(130, 33);
+            this.bntIngresar.TabIndex = 8;
+            this.bntIngresar.Text = "Ingresar";
+            this.bntIngresar.UseVisualStyleBackColor = false;
+            this.bntIngresar.Click += new System.EventHandler(this.bntIngresar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(313, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "INICIAR SESION";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button3.Image = global::ProyectoPA.Properties.Resources.informacion;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(669, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 56);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "   INFORMACION";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_2);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Image = global::ProyectoPA.Properties.Resources.hogar;
+            this.pictureBox2.Location = new System.Drawing.Point(37, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(44, 28);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button2.Image = global::ProyectoPA.Properties.Resources.avatar;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(3, 304);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.button2.Size = new System.Drawing.Size(213, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "   CHOFERES";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button1.Image = global::ProyectoPA.Properties.Resources.usuario__3_;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(3, 240);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.button1.Size = new System.Drawing.Size(213, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "   ADMINISTRACION";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::ProyectoPA.Properties.Resources.Red_Bus_PNG_Clipart_559;
+            this.pictureBox1.Image = global::ProyectoPA.Properties.Resources.Red_Bus_PNG_Clipart_1000;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 538);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelTitulo);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.panelTitulo.ResumeLayout(false);
+            this.panelTitulo.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panelTitulo;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblTitulo;
+        public System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.TextBox txtUsuario;
+        public System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.Button bntIngresar;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ProyectoPA/Form1.cs
@@ -0,0 +1,117 @@
+﻿using ProyectoPA.BD;
+using ProyectoPA.Formularios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPA
+{
+    public partial class Form1 : Form
+    {
+        frmAdmin admin = new frmAdmin();
+        frmChofer chof = new frmChofer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void panelAdmin()
+        {
+            frmAdmin a = new frmAdmin();
+            Algoritmos.Algoritmos.addFormulario(panel2, a);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        DataClasses1DataContext DB = new DataClasses1DataContext();
+
+        private void bntIngresar_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contra = txtContra.Text;
+
+            var user = (from s in DB.Sesion
+                        where s.Usuario == txtUsuario.Text
+                        select s).First();
+            try
+            {
+
+            if (user.Contrasena == txtContra.Text && user.Usuario == txtUsuario.Text && user.Tipo == "Admin")
+            {
+                MessageBox.Show("Bienvenido " + user.Usuario + " :D");
+                txtContra.Text = "";
+                txtUsuario.Text = "";
+                lblTitulo.Text = "ADMINISTRACION   BIENVENIDO " + usuario;
+                pictureBox2.Image = Image.FromFile("usuario (3).png");
+                button1.BackColor = Color.FromArgb(56, 54, 128);
+                chof.Hide();
+                admin.Show();
+            }
+            else if (user.Contrasena == txtContra.Text && user.Usuario == txtUsuario.Text && user.Tipo == "Chofer")
+            {
+                var chofer = (from s in DB.Chofer
+                                where s.Cedula == txtUsuario.Text
+                                select s).First();
+                MessageBox.Show("Bienvenido " + chofer.Nombres + " :D");
+                    txtContra.Text = "";
+                    txtUsuario.Text = "";
+                    lblTitulo.Text = "CHOFERES    BIENVENIDO " + chofer.Nombres;
+                    pictureBox2.Image = Image.FromFile("avatar.png");
+                    button2.BackColor = Color.FromArgb(56, 54, 128);
+                    admin.Hide();
+                    chof.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Error al iniciar sesion, rectifique los datos");
+                    txtContra.Text = "";
+                    txtUsuario.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al iniciar sesion, porfavor intente de nuevo");
+                txtContra.Text = "";
+                txtUsuario.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmSesion a = new frmSesion();
+            a.Show();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            frmSesion a = new frmSesion();
+            a.Show();
+        }
+
+        private void button3_Click_2(object sender, EventArgs e)
+        {
+            Process op = new Process();
+            op.StartInfo.FileName = "C:\\Users\\ELIAN\\Documents\\AdministradorDeBusesCoop.DeBusesMachalaCity.chm";
+            op.Start();
+        }
+    }
+}
